83f936b267e98569a8109344de3a98f308a0c4ed
FabMira <fmirabella08@gmail.com>
2024-06-08 15:25:46 -0400

    first webapp index

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hooa munda")
+const express = require('express')
+const app = express()
+
+let persons = [
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>hello world</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`is ${PORT}`)
+})
