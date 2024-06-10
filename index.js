a0bbda5a727ab3a165d10bb5c4bed1dd643141bc
FabMira <fmirabella08@gmail.com>
2024-06-09 21:59:29 -0400

    dist to backend folder

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ morgan.token('reqContent', function getContent (req) {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :reqContent'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
